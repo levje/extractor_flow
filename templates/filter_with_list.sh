025499b1c3b0d107540993d7da15679347e26cb9
Jeremi Levesque <jeremilevesque@hotmail.com>
2025-02-19 16:30:46 -0500

    allow empty

--- templates/filter_with_list.sh
+++ templates/filter_with_list.sh
@@ -15,7 +15,8 @@ if [ "${keep}" = "true" ]
 then
     scil_tractogram_math.py difference ${tractogram} \
                                         ${basename}__${out_extension}.trk \
-                                        ${sid}__${remaining_extension}.trk ;
+                                        ${sid}__${remaining_extension}.trk \
+                                        --save_empty;
     scil_count_streamlines.py ${sid}__${remaining_extension}.trk > ${sid}__${remaining_extension}.txt;
 fi
  
